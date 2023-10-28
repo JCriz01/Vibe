25607959cc5ad220a09721e7bd44a1b01dad3943
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2023-10-28 15:59:55 -0400

    Implemented 10mb limit for json data

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
 import express from "express";
+//import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import connectDB from "./database/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //middlewares
-app.use(express.json()); //Parsing Json data to req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(express.json({ limit: "10mb" })); //Parsing Json data to req.body
+app.use(express.urlencoded({ limit: "10mb", extended: true })); // To parse form data in the req.body
 app.use(cookieParser()); //read and access cookies
 
 //routes
