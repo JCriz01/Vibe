979a90ad254553d01b8f1ce37bd91b7550a1975d
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2023-10-28 21:51:23 -0400

    Implemented Header buttons for profile and home pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,37 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react"
+import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { Link as RouterLink } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 
-const Header = ()=>{
+const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
 
-    const {colorMode, toggleColorMode}=useColorMode()
-    return (
-        <Flex justifyContent={'center'} mt={6} mb={'12'}>
+  return (
+    <Flex justifyContent={user ? "space-between" : "center"} mt={6} mb={"12"}>
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={7}
+        src={colorMode === "light" ? "/social.png" : "/social-white.png"}
+        onClick={toggleColorMode}
+      />
 
-            <Image 
-                cursor={"pointer"}
-                alt="logo"
-                w={7}
-                src={colorMode === "light" ? '/social.png': '/social-white.png'}
-                onClick={toggleColorMode}
-            />
-        </Flex>
-    )
-}
+      {user && (
+        <Link as={RouterLink} to={`/${user.username}`}>
+          <RxAvatar size={24} />
+        </Link>
+      )}
+    </Flex>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,55 @@
 import UserHeader from "../components/UserHeader";
 import UserPosts from "../components/UserPosts";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+
+        setUser(data);
+      } catch (error) {
+        showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getUser();
+  }, [username, showToast]);
+
+  if (!user && loading) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+  }
+  if (!user && !loading) {
+    return <div>User not found</div>;
+  }
+  if (!user) {
+    return null;
+  }
+
   return (
     <>
-      <UserHeader />
+      <UserHeader user={user} />
 
       <UserPosts
         likes={2}
