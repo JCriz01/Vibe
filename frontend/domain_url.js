25942288adb2ea77ddffa281691e70b3b40a068c
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2023-11-10 22:56:15 -0500

    fixing port issue with backend domain url

--- frontend/domain_url.js
+++ frontend/domain_url.js
@@ -1,3 +1,4 @@
 //https://vibe-production.up.railway.app
 //js file to store the domain url
-export const domainUrl = "https://vibe-production.up.railway.app:5500";
+
+export const domainUrl = "https://vibe-production.up.railway.app";
