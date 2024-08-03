59f19c0f75d95d4d83c01638ebdd542ad4e2637b
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2024-08-03 19:32:14 -0400

    Implement Prisma models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -8,10 +8,10 @@ import connectToMongoDB from "./database/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({path: path.join(__dirname, "/.env")});
+dotenv.config({ path: path.join(__dirname, "/.env") });
 
 //connecting to database
 connectToMongoDB();
@@ -26,29 +26,32 @@ const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN;
 console.log(FRONTEND_DOMAIN);
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-let allowedOrigins = ["https://vibe-production-6a25.up.railway.app"];
+let allowedOrigins = [
+  "https://vibe-production-6a25.up.railway.app",
+  BACKEND_DOMAIN,
+];
 
 //use cors to allow cross origin resource sharing
 app.use(
-	cors({
-		origin: function (origin, callback) {
-			if (!origin) return callback(null, true);
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
 
-			if (allowedOrigins.indexOf(origin) === -1) {
-				let message =
-					"The CORS policy for this site does not allow access from the specified Origin.";
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		credentials: true,
-	})
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 //use helmet to secure the app by setting various HTTP headers
@@ -58,13 +61,13 @@ app.use(helmet());
 app.use(cookieParser());
 
 //middlewares
-app.use(express.json({limit: "10mb"})); //Parsing Json data to req.body
-app.use(express.urlencoded({limit: "10mb", extended: true})); // To parse form data in the req.body
+app.use(express.json({ limit: "10mb" })); //Parsing Json data to req.body
+app.use(express.urlencoded({ limit: "10mb", extended: true })); // To parse form data in the req.body
 
 //routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
 app.listen(PORT, "0.0.0.0", () =>
-	console.log(`Server started at ${BACKEND_DOMAIN}:${PORT}`)
+  console.log(`Server started at ${BACKEND_DOMAIN}:${PORT}`)
 );
