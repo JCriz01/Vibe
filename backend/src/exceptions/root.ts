69c039f98a37e0a17631b9c4675de130ef978678
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2024-08-07 15:24:02 -0400

    code custom global error handler

---
+++ backend/src/exceptions/root.ts
@@ -0,0 +1,32 @@
+// structure of the error response
+// message, status, code, error codes
+
+export class HttpException extends Error {
+  message: string;
+  errorCode: unknown;
+  statusCode: number;
+  errors: ErrorCodes | null;
+
+  constructor(
+    message: string,
+    errorCode: unknown,
+    statusCode: number,
+    errors: ErrorCodes | null
+  ) {
+    super(message);
+    this.message = message;
+    this.errorCode = errorCode;
+    this.statusCode = statusCode;
+    this.errors = errors;
+  }
+}
+
+export enum ErrorCodes {
+  USER_NOT_FOUND = 1001,
+  USER_ALREADY_EXISTS = 1002,
+  USER_FOLLOW_ERROR = 1003,
+  POST_NOT_FOUND = 2001,
+  POST_CREATION_ERROR = 2002,
+  COMMENT_NOT_FOUND = 3001,
+  UNPROCESSABLE_ENTITY = 4001,
+}
