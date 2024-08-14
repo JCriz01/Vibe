0dbc17dccf57e428b1b9214c30134ad33dd940b5
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2024-08-14 16:13:43 -0400

    Adding vite typescript & tailwindcss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 //const FRONTEND_PORT = process.env.PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
