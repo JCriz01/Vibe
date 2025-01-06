c798ce42fc5a75f1fb76e427b2f6ea91e750280f
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2025-01-05 19:51:49 -0500

    removed Chakra UI

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { useAtom } from "jotai";
 //import { useSetRecoilState } from "recoil";
@@ -29,6 +29,11 @@ import { Link } from "react-router-dom";
 
 import { useToast } from "@/components/ui/use-toast";
 
+type Inputs = {
+  exampe: string;
+  exampleRequired: string;
+};
+
 const LoginForm = () => {
   const navigate = useNavigate();
   const [, setAuthScreen] = useAtom(authScreenAtom);
@@ -39,7 +44,13 @@ const LoginForm = () => {
   //const showToast = useShowToast();
   const { toast } = useToast();
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  //adding form that may replace current form data
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
@@ -47,6 +58,16 @@ const LoginForm = () => {
     },
   });
 
+  //remove this later
+  /*
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  */
   async function onSubmit(formData: z.infer<typeof formSchema>) {
     //do something with the values
     console.log(formData);
@@ -65,10 +86,10 @@ const LoginForm = () => {
       });
       const data = await res.json();
       console.log(data);
-      if (data.error) {
+      if (data.errorCode) {
         toast({
-          title: "Error",
-          description: data.error,
+          title: "Sign in error",
+          description: data.message,
           variant: "destructive",
         });
         return;
@@ -101,72 +122,69 @@ const LoginForm = () => {
   return (
     <div className="flex flex-col items-center justify-center flex-cov h-full">
       <div className=" flex flex-col items-center mb-3 gap-4">
-        <h2 className=" dark:text-white text-center self-center text-4xl font-bold">Login to your account</h2>
+        <h2 className=" dark:text-white text-center self-center text-4xl font-bold">
+          Sign into your account
+        </h2>
         <p className=" text-gray-600 font-sans text-lg">
           To have fun with the gang
         </p>
       </div>
-      <Card className="p-3 md:w-3/5 lg:w-[520px] dark:bg-white">
+      <Card className="p-3 md:w-3/5 lg:w-[520px] dark:bg-slate-900">
         <CardContent className="flex flex-col ">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-8 p-3 flex flex-col dark:text-gray-900"
-            >
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input
-                        className=" dark:text-gray-200 bg-white "
-                        placeholder="username"
-                        {...field}
-                      />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className=" p-3 flex flex-col dark:text-white space-y-8"
+          >
+            <div className="space-y-2">
+              <label htmlFor="Username">Username</label>
+              {/* register your input into the hook by invoking the "register" function */}
+              <input
+                placeholder="username"
+                {...register("username")}
+                className="flex h-10 w-full rounded-md border border-slate-200 dark:bg-slate-900 px-3 py-2 text-sm ring-offset-white"
               />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input className=" dark:text-gray-200 " {...field} />
-                    </FormControl>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="flex items-center justify-center">
-                <p className=" self-center dark:text-gray-900 text-center">Dont have an account? {" "}
-                  <Link to={"/"} onClick={() => setAuthScreen("register")}>
-                    <p className={"text-blue-500"}>Sign Up</p>
-                  </Link>
-                </p>
-              </div>
-              {loading ? (
-                <Button className="self-center" type="submit" disabled>
-                  <l-ring />
-                </Button>
-              ) : (
-                <Button className="self-center dark:bg-neutral-500" type="submit">
-                  <p className={"dark:text-white"}>Login In</p>
-                </Button>
+              {errors.username && (
+                <span className="text-red-800">{errors.username.message}</span>
               )}
-            </form>
-          </Form>
+            </div>
+
+            <div className="space-y-2">
+              <label htmlFor="password">Password</label>
+              {/* include validation with required or other standard HTML validation rules */}
+              <input
+                {...register("password", { required: true })}
+                className="flex h-10 w-full rounded-md border border-slate-200 dark:bg-slate-900 px-3 py-2 text-sm ring-offset-white"
+                placeholder="password"
+              />
+              {/* errors will return when field validation fails  */}
+              {errors.password && (
+                <span className="text-red-800">{errors.password.message}</span>
+              )}
+            </div>
+            <div className="flex items-center justify-center">
+              <p className=" self-center dark:white text-center">
+                Dont have an account?{" "}
+                <Link to={"/"} onClick={() => setAuthScreen("register")}>
+                  <span className={"text-blue-500"}>Sign Up</span>
+                </Link>
+              </p>
+            </div>
+            {loading ? (
+              <Button className="self-center" type="submit" disabled>
+                <l-ring />
+              </Button>
+            ) : (
+              <Button
+                className="self-center dark:bg-cyan-900 w-full p-4"
+                type="submit"
+              >
+                <p className={"dark:text-white font-medium"}>Login In</p>
+              </Button>
+            )}
+          </form>
         </CardContent>
       </Card>
     </div>
   );
 };
-
 export default LoginForm;
